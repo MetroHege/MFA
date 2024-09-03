35c0e2268744dab05fac2979f40a258a0f78e65b
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-09-03 11:58:01 +0300

    update

--- MFA-starter-main/src/api/controllers/twoFAController.ts
+++ MFA-starter-main/src/api/controllers/twoFAController.ts
@@ -1,11 +1,12 @@
-import {UserResponse} from '@sharedTypes/MessageTypes';
+import {LoginResponse, UserResponse} from '@sharedTypes/MessageTypes';
 import {NextFunction, Request, Response} from 'express';
 import CustomError from '../../classes/CustomError';
-import {User} from '@sharedTypes/DBTypes';
+import {TokenContent, User, UserWithLevel} from '@sharedTypes/DBTypes';
 import fetchData from '../../utils/fetchData';
 import OTPAuth from 'otpauth';
 import twoFAModel from '../models/twoFAModel';
 import QRCode from 'qrcode';
+import jwt from 'jsonwebtoken';
 // TODO: Import necessary types and models
 
 // Define setupTwoFA function
@@ -62,18 +63,75 @@ const setupTwoFA = async (
   }
 };
 
-// TODO: Define verifyTwoFA function
-// const verifyTwoFA = async (req, res, next) => {
-//   const {email, code} = req.body;
+// Define verifyTwoFA function
+const verifyTwoFA = async (
+  req: Request<{}, {}, {email: string; code: string}>,
+  res: Response<LoginResponse>,
+  next: NextFunction,
+) => {
+  const {email, code} = req.body;
 
-//   try {
-//     // TODO: Retrieve 2FA data from the database
-//     // TODO: Validate the 2FA code
-//     // TODO: If valid, get the user from AUTH API
-//     // TODO: Create and return a JWT token
-//   } catch (error) {
-//     next(new CustomError((error as Error).message, 500));
-//   }
-// };
+  try {
+    // Retrieve 2FA data from the database
+    const twoFactorData = await twoFAModel.findOne({email: email});
+    if (!twoFactorData || !twoFactorData.twoFactorEnabled) {
+      next(new CustomError('2FA is not enabled for this user', 400));
+      return;
+    }
 
-export {setupTwoFA};
+    console.log('twoFactorData:', twoFactorData);
+
+    // Validate the 2FA code
+    const totp = new OTPAuth.TOTP({
+      issuer: 'MFAtesti',
+      label: email,
+      algorithm: 'SHA1',
+      digits: 6,
+      period: 30,
+      secret: OTPAuth.Secret.fromBase32(twoFactorData.twoFactorSecret),
+    });
+
+    const isValid = totp.validate({
+      token: code,
+      window: 1,
+    });
+
+    if (!isValid) {
+      next(new CustomError('Invalid 2FA code', 400));
+      return;
+    }
+
+    // If valid, get the user from AUTH API
+    const UserResponse = await fetchData<UserWithLevel>(
+      process.env.AUTH_URL + '/api/v1/users/' + twoFactorData.userId,
+    );
+
+    if (!UserResponse) {
+      next(new CustomError('User not found', 404));
+      return;
+    }
+
+    // Create and return a JWT token
+    const tokenContent: TokenContent = {
+      user_id: twoFactorData.userId,
+      level_name: UserResponse.level_name,
+    };
+
+    if (!process.env.JWT_SECRET) {
+      throw new Error('JWT_SECRET is not defined');
+    }
+
+    const token = jwt.sign(tokenContent, process.env.JWT_SECRET);
+    const loginResponse: LoginResponse = {
+      user: UserResponse,
+      token: token,
+      message: 'Login successful',
+    };
+
+    res.json(loginResponse);
+  } catch (error) {
+    next(new CustomError((error as Error).message, 500));
+  }
+};
+
+export {setupTwoFA, verifyTwoFA};
